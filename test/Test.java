187000be3773f6a6dd37f6064aae2e0d201629d8
aiuar456 <austin.letson@gmail.com>
2016-08-30 22:45:16 -0400

    Added shell for testing

---
+++ test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+import src.StandardTestSystem;
+
+/**
+ * Test suite for tengine
+ *
+ * @author austinletson
+ */
+public class Test{
+
+    public static void main(String[] args){
+
+    }
+}
